3aa90e0c700dd8ed388e316c9a319fd61b5c078e
vxav <xavier.avrillier@gmail.com>
2016-06-15 20:09:30 +0100

    Add files via upload

---
+++ Get-VMCPULimit.ps1
@@ -0,0 +1,17 @@
+﻿Function Get-VMCpuLimit {
+
+Param(
+     [parameter(position=0,ValueFromPipeline=$True,ValueFromPipelineByPropertyname=$True,Mandatory=$True)]
+     [VMware.VimAutomation.ViCore.types.V1.Inventory.VirtualMachine[]]
+     $VM
+)
+
+$VM | ForEach-Object {
+    
+    [pscustomobject]@{
+        VM    = $_.name
+        Limit = $_.ExtensionData.Config.CpuAllocation.Limit
+    }
+}
+
+}
