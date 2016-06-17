ef65944ec31c0bc4e62593ea6d8d2adf2898e854
vxav <xavier.avrillier@gmail.com>
2016-06-17 13:55:44 +0100

    Add files via upload

---
+++ Get-Datastore2.ps1
@@ -0,0 +1,26 @@
+﻿Function Get-Datastore2 {
+
+param(
+    [parameter(position=0,ValueFromPipeline=$True,ValueFromPipelineByPropertyname=$True)]
+    [VMware.VimAutomation.ViCore.Types.V1.DatastoreManagement.VmfsDatastore[]]
+    $Datastore
+)
+
+Process{
+
+    $Datastore | ForEach-Object {
+
+        [pscustomobject]@{
+            Name          = $_.name
+            CapacityGB    = [Math]::Round(($_.extensiondata.summary.capacity   / 1GB),2)
+            FreeSpaceGB   = [Math]::Round(($_.extensiondata.summary.FreeSpace  / 1GB),2)
+            UsedSpaceGB   = [Math]::Round((($_.extensiondata.summary.capacity  / 1GB) - ($_.extensiondata.summary.FreeSpace / 1GB)),2)
+            ProvisionedGB = [Math]::Round((($_.extensiondata.summary.capacity  / 1GB) - ($_.extensiondata.summary.FreeSpace / 1GB) + ($_.extensiondata.summary.Uncommitted / 1GB)),2)
+            NbRunningVMs  = ($_ | Get-VM | where powerstate -eq Poweredon).count
+        }
+
+    }
+
+}
+
+}
---
+++ Get-ThinHardDisk.ps1
@@ -0,0 +1,20 @@
+﻿Function Get-ThinHardDisk {
+
+param(
+    [parameter(position=0,ValueFromPipeline=$True,ValueFromPipelineByPropertyname=$True)]
+    [ValidateNotNullOrEmpty()]
+    [VMware.VimAutomation.ViCore.types.V1.Inventory.VirtualMachine[]]
+    $VM = (get-VM)
+)
+
+Process{
+
+    $VM | ForEach-Object {
+
+        (Get-HardDisk -VM ($_) | where StorageFormat -eq Thin) | select Parent,Name,CapacityGB,StorageFormat
+
+    }
+
+}
+
+}
