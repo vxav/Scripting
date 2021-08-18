82622f35686c5daac750e266b1749acf527936ca
Xavier <xavier.avrillier@gmail.com>
2021-08-18 12:01:45 +0100

    Update Rdcman.psm1

--- Rdcman.psm1
+++ Rdcman.psm1
@@ -8,10 +8,18 @@ Create an "RDCMan ready" RDG file with all the running Windows VMs connected in 
 - The VM must have the VMware tools installed.
 - RDCMan must be closed.
 
-[ Update 201808/17 ]
+### CHANGE LOG
 
-- Added 3389 TCP port check on VM IP(s) - Set the first one the responds as hostname and "NO-RDP-RESPONSE" if none does.
-- Added TCPCheckTimeout parameter to Add-RDCManVM function to configure the time it takes for the TCP port check to time out.
+    01/03/2019
+    ! Bug found - The RDG file must be opened at least once before running the set command to update it
+
+    07/11/2018
+    - [Set-RDCManFile] Adds new VMs and new domains to an existing file.
+    - Added Function Set-RDCManFile.
+
+    17/08/2018
+    - Added 3389 TCP port check on VM IP(s) - Set the first one the responds as hostname and "NO-RDP-RESPONSE" if none does.
+    - Added TCPCheckTimeout parameter to Add-RDCManVM function to configure the time it takes for the TCP port check to time out.
 
 #>
 
@@ -31,7 +39,7 @@ param(
 )
 
 # RDCMan must be closed otherwise the changes won't persist
-if (get-process RDCMan -ErrorAction SilentlyContinue) {Write-Warning "Close RDCMan before running editting the file"; break}
+if (get-process RDCMan -ErrorAction SilentlyContinue) {Write-Warning "Close RDCMan before editing the file"; break}
 
 # Grab rdg file
 $rdgfile    = Get-ChildItem $RdgFilePath
@@ -45,6 +53,8 @@ $VM = $VM | where {$_.powerstate -eq "poweredon" -and $_.guest.OSFullName -match
 foreach ($V in $VM) {
 
     if ($v.guest.OSFullName -match "windows" -and !($RdgContent -match "<displayName>$($v.guest.HostName)</displayName>") -and !($RdgContent -match "<name>$IP</name>")) {
+        
+        if ($v.guest.State -eq "Running") { # Tools running
 
         $Prog++
 
@@ -53,7 +63,7 @@ foreach ($V in $VM) {
         $group = $group.ToLower()
 
         # Check if group exists
-        if (!($RdgContent -match "<name>$Group</name>")) {Write-Warning "Group $Group found in specified rdg file; $($V.name) skipped"; break}
+        if (!($RdgContent -match "<name>$Group</name>")) {Write-Warning "Group $Group not found in specified rdg file; $($V.name) skipped"; break}
 
 
         Write-Progress -Activity "Group $Group : VM #$Prog/$($VM.count)" -Status $V.name -PercentComplete ($Prog/$VM.count*100) -Id 1
@@ -94,6 +104,7 @@ foreach ($V in $VM) {
           <name>$($3389)</name>
           <comment>$($v.guest.HostName)
 VM     : $($v.name)
+vCenter: $($DefaultVIServer.name)
 Cluster: $($v | get-cluster | select -ExpandProperty name)
 IP(s)  : $([string]$IP)
 OS     : $($v.guest.OSFullName)
@@ -105,8 +116,10 @@ $($VMNotes -replace "@",' at ' -replace "<","" -replace ">","" -replace "&"," an
 "@
     
     Clear-Variable i,3389 -ErrorAction SilentlyContinue
+
+    } else {Write-Warning "Tools not running on $($v.name)"} # Tools running
     
-    } else {Write-warning "$($v.name): non windows host or already in rdg file"}
+    } else {Write-warning "$($v.name): not a windows host or already in rdg file"}
 
 }
 
@@ -128,6 +141,21 @@ $RdgContent | Out-File $rdgfile -Confirm:$false -Encoding utf8
 
 }
 
+Function Start-RDCMan {
+
+param(
+    [System.IO.FileInfo]
+    $RdgFile
+)
+
+"Opening $RdgFile in RDCMan (required before using Set-RDCManFile)"
+
+Start-Process $RdgFile
+
+While ( !(get-process RDCMan -ErrorAction SilentlyContinue) ) {sleep -Milliseconds 200}
+
+}
+
 Function New-RDCManFile {
 
 param(
@@ -139,10 +167,17 @@ param(
     $VM
 )
 
-Write-Progress -Activity "Creating base rdg file"
+# RDCMan must be closed otherwise the changes won't persist
+if (get-process RDCMan -ErrorAction SilentlyContinue) {Write-Warning "Close RDCMan before editing the file"; break}
+
+Write-Progress -Activity "Gathering VM and Domain lists"
 $VM = $VM | where {$_.powerstate -eq "poweredon" -and $_.guest.OSFullName -match "windows"}
 $Domains = $VM | select @{l="domain";e={$_.guest.ExtensionData.IpStack.dnsconfig.domainname}} | where domain | select -ExpandProperty domain -Unique domain
 
+Write-Progress -Activity "Creating base rdg file"
+$RDGFile = New-Item -Path $RdgFilePath -ItemType File -Force
+
+# Prepare the no-domain section
 $DomTxt = @"
     <group>
       <properties>
@@ -152,7 +187,8 @@ $DomTxt = @"
     </group>
 "@
 
-foreach ($dom in $Domains) {
+# Prepare each domain's section
+foreach ($dom in $Domains.tolower()) {
 $DomTxt += @"
     <group>
       <properties>
@@ -163,6 +199,7 @@ $DomTxt += @"
 "@
 }
 
+# Fit the prepared section in the global rdg frame and write to file
 @"
 <?xml version="1.0" encoding="utf-8"?>
 <RDCMan programVersion="2.7" schemaVersion="3">
@@ -170,7 +207,7 @@ $DomTxt += @"
     <credentialsProfiles />
     <properties>
       <expanded>True</expanded>
-      <name>$($DefaultVIServer.name)</name>
+      <name>$($RDGFile.BaseName)</name>
     </properties>
     <remoteDesktop inherit="None">
       <sameSizeAsClientArea>True</sameSizeAsClientArea>
@@ -191,6 +228,76 @@ ForEach ($VMDomain in $Domains) {
 
     Write-Progress -Activity "Domain #$a/$($Domains.count)" -Status $VMDomain -PercentComplete ($a/$Domains.count*100)
 
+    # Gather all VMs that are part of the domain being currently processed
+    $V = $VM | where {$_.guest.ExtensionData.IpStack.dnsconfig.domainname -eq $VMDomain}
+
+    # Add gathered VMs to the created RDG file
+    Add-RDCManVM -RdgFilePath $RdgFilePath -VM $V
+
+}
+
+Write-Progress -Activity "Domain No-Domain : Domain #$a/$($Domains.count)" -Status "No-Domain" -PercentComplete ($a++/$Domains.count*100) -Id 0
+
+$VMNoDomain = $VM | where {!($_.guest.ExtensionData.IpStack.dnsconfig.domainname)}
+Add-RDCManVM -RdgFilePath $RdgFilePath -VM $VMNoDomain
+
+"-End-"
+
+Start-RDCMan -RdgFilePath $RdgFile
+
+}
+
+Function Set-RDCManFile {
+
+param(
+    [System.IO.FileInfo]
+    $RdgFilePath,
+    
+    [parameter(position=0)]
+    [VMware.VimAutomation.ViCore.types.V1.Inventory.VirtualMachine[]]
+    $VM
+)
+
+if ( !($RDGFile = Get-ChildItem $RdgFilePath) ) { Break }
+
+# RDCMan must be closed otherwise the changes won't persist
+if (get-process RDCMan -ErrorAction SilentlyContinue) {Write-Warning "Close RDCMan before editing the file"; break}
+
+Write-Progress -Activity "Updating base rdg file"
+$VM = $VM | where {$_.powerstate -eq "poweredon" -and $_.guest.OSFullName -match "windows"}
+$Domains = $VM | select @{l="domain";e={$_.guest.ExtensionData.IpStack.dnsconfig.domainname}} | where domain | select -ExpandProperty domain -Unique domain
+
+[xml]$DataXML = Get-content $RdgFilePath
+
+# Enter loop for each detected domain
+foreach ($dom in $Domains.tolower()) {
+
+# If current domain not in rdg file
+if ($DataXML.rdcman.file.group.properties.name -notcontains $dom) {
+    $DomTxt += @"
+    <group>
+      <properties>
+        <expanded>False</expanded>
+        <name>$dom</name>
+      </properties>
+    </group>
+"@
+}
+
+}
+
+# Add sections for new domains in the rdg file
+if ($DomTxt) {
+    (Get-content $RDGFile) -replace "</file>","$DomTxt
+    </file>" | Out-File $RdgFile
+}
+
+ForEach ($VMDomain in $Domains) {
+    
+    $a++
+
+    Write-Progress -Activity "Domain #$a/$($Domains.count)" -Status $VMDomain -PercentComplete ($a/$Domains.count*100)
+
     $V = $VM | where {$_.guest.ExtensionData.IpStack.dnsconfig.domainname -eq $VMDomain}
 
     Add-RDCManVM -RdgFilePath $RdgFilePath -VM $V
@@ -203,4 +310,8 @@ $VMNoDomain = $VM | where {!($_.guest.ExtensionData.IpStack.dnsconfig.domainname
 Add-RDCManVM -RdgFilePath $RdgFilePath -VM $VMNoDomain
 
 "-End-"
+
+Start-RDCMan -RdgFile $RdgFilePath
+
 }
+
